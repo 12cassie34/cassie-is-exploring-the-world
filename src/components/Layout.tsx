4c8b1072a16324d9bf15ee53e85aea3eb005abfe
Ines Lin <ineslin35@gmail.com>
2022-12-13 00:01:39 +0800

    refactor: use Grid and GridItem from chakra to produce the layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,28 @@
 import React, { FunctionComponent, PropsWithChildren } from "react"
 
 import { Box } from '@chakra-ui/react'
+import { Grid } from '@chakra-ui/react'
 
 import theme from "../@chakra-ui/gatsby-plugin/theme"
+
 
 import Header from "./Header"
 
 const Layout: FunctionComponent<PropsWithChildren> = ({ children }) => {
 
     return (
-        <Box bg={theme.colors.customs.bg.light} w='100%' h='100%'>
-            <Header />
+        <Grid
+            templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+            gridTemplateColumns={'150px 1fr'}
+            h='100%'
+            gap='1'
+            color={theme.colors.customs.bg.light}
+            fontWeight='bold'
+        >
             {children}
-        </Box>
+        </Grid>
     )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,25 @@
-import * as React from "react"
-import type { HeadFC, PageProps } from "gatsby"
+import React, { FC } from "react"
+import type { PageProps } from "gatsby"
+
+import { Grid, GridItem } from '@chakra-ui/react'
 
 import Layout from "../components/Layout"
+import Header from "../components/Header"
 
-const IndexPage: React.FC<PageProps> = () => {
+const IndexPage: FC<PageProps> = () => {
   return (
     <Layout>
-      <main>
-        this is going to be the child-layout
-      </main>
+      <Header />
+      {/* <GridItem pl='2' bg='pink.300' area={'nav'}>
+        Nav
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'footer'}>
+        Footer
+      </GridItem> */}
     </Layout>
-
   )
 }
 
