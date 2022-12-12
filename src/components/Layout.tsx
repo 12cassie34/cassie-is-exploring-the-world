88cabcbeac062952e187fe128a019566250ff897
Ines Lin <ineslin35@gmail.com>
2022-12-12 20:10:40 +0800

    feat: add Layout of the app

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import { FunctionComponent } from "react";
+import * as React from "react"
+
+import { Box } from '@chakra-ui/react'
+
+const Layout: FunctionComponent = () => {
+
+    return (
+        <Box bg='tomato' w='100%' h='100%' p={4} color='white'>
+            This is the Box
+        </Box>
+    )
+}
+
+export default Layout;
