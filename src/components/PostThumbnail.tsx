8e958206c38f858dcdbdca4a2e3f03fc9f2cb3fc
Shan Yu Lin <ineslin35@gmail.com>
2022-12-13 22:57:49 +0800

    feat: use PostThumbnail in Main area instead of in SideMenu component

--- src/components/PostThumbnail.tsx
+++ src/components/PostThumbnail.tsx
@@ -1,44 +1,58 @@
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent } from 'react';
 
-import { Avatar, Card, CardBody, Divider, Heading, Image, Link, Stack, Text } from '@chakra-ui/react'
+import { Divider, Grid, GridItem, Heading, Image, Link, Stack, Text } from '@chakra-ui/react';
 
-import theme from "../@chakra-ui/gatsby-plugin/theme"
+import theme from '../@chakra-ui/gatsby-plugin/theme';
 
 export interface PostThumbnailProps {
-    title: string
-    avatarSrc: string
-    date: string
-    summary: string
-    articleLink: string
+  title: string;
+  bannerSrc: string;
+  date: string;
+  summary: string;
+  articleLink: string;
 }
 
-const PostThumbnail: FunctionComponent<PostThumbnailProps> = ({ title, avatarSrc, date, summary, articleLink }) => {
-    return (
-        <>
-            <Card pl={0} pt={4} pb={6} maxW="sm" boxShadow="none">
-                <CardBody p={2}>
-                    <Image maxW={{ base: '100%', sm: '300px' }} fallbackSrc={avatarSrc} borderRadius='lg' />
-                    <Stack mt={6} spacing={2}>
-                        <Heading as='h6' size='xs' color={theme.colors.customs.text.light}>
-                            {date}
-                        </Heading>
-                        <Link href={articleLink} isExternal color={theme.colors.customs.text.dark}>
-                            <Heading size='md'>{title}</Heading>
-                        </Link>
-                        <Text maxHeight={100} overflow="hidden" color={theme.colors.customs.text.middle}>
-                            {summary}
-                        </Text>
-                        <Link href={articleLink} isExternal>
-                            <Heading as='h6' size='xs' color={theme.colors.customs.text.light}>
-                                Read More
-                            </Heading>
-                        </Link>
-                    </Stack>
-                </CardBody>
-            </Card>
-            <Divider />
-        </>
-    )
-}
+const PostThumbnail: FunctionComponent<PostThumbnailProps> = ({
+  title,
+  bannerSrc,
+  date,
+  summary,
+  articleLink
+}) => {
+  return (
+    <>
+      <Grid h={200} templateRows="repeat(1, 1fr)" templateColumns="repeat(12, 1fr)" gap={6}>
+        <GridItem w="100%" colSpan={3} rowSpan={2} display="flex">
+          <Image
+            width={300}
+            objectFit="scale-down"
+            fallbackSrc={bannerSrc}
+            borderRadius="lg"
+            alt={title}
+          />
+        </GridItem>
+        <GridItem w="100%" colSpan={9} rowSpan={2}>
+          <Stack mt={6} spacing={2}>
+            <Heading as="h6" size="xs" color={theme.colors.customs.text.light}>
+              {date}
+            </Heading>
+            <Link href={articleLink} isExternal color={theme.colors.customs.text.dark}>
+              <Heading size="md">{title}</Heading>
+            </Link>
+            <Text maxHeight={100} overflow="hidden" color={theme.colors.customs.text.middle}>
+              {summary}
+            </Text>
+            <Link href={articleLink} isExternal>
+              <Heading as="h6" size="xs" color={theme.colors.customs.text.light}>
+                Read More
+              </Heading>
+            </Link>
+          </Stack>
+        </GridItem>
+      </Grid>
+      <Divider />
+    </>
+  );
+};
 
-export default PostThumbnail
+export default PostThumbnail;
