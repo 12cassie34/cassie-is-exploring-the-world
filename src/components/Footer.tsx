8263e9170076320dd3bacb96667792cceb595ee7
Ines Lin <ineslin35@gmail.com>
2022-12-17 19:29:40 +0800

    feat: add Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,31 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useRef } from 'react';
 
-import { Heading, GridItem } from '@chakra-ui/react';
+import { Flex, Heading, GridItem, Spacer } from '@chakra-ui/react';
 
 import theme from '../@chakra-ui/gatsby-plugin/theme';
 
-const Footer: FunctionComponent = () => (
-  <GridItem
-    rowSpan={1}
-    colSpan={12}
-    p="8"
-    bg={theme.colors.customs.bg.middle}
-    color={theme.colors.customs.text.middle}
-  >
-    <Heading letterSpacing={2}>Cassie is exploring the world</Heading>
-  </GridItem>
-);
+const Footer: FunctionComponent = () => {
+  const year = useRef(new Date().getFullYear());
+
+  return (
+    <GridItem
+      colSpan={12}
+      mt={-2}
+      p="8"
+      bg={theme.colors.customs.bg.dark}
+      color={theme.colors.customs.text.light}
+    >
+      <Flex>
+        <Heading letterSpacing={2} as="h6" size="xs">
+          Cassie is exploring the world
+        </Heading>
+        <Spacer />
+        <Heading letterSpacing={2} as="h6" size="xs">
+          2023-{year.current}
+        </Heading>
+      </Flex>
+    </GridItem>
+  );
+};
 
 export default Footer;
